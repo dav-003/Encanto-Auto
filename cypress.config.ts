27a7c4f6db145ee32711a4364af3fc275f27dabd
Karen <kmkrtchyan335@gmail.com>
2025-06-09 18:50:44 +0400

    New commit

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://192.168.3.225:3000/',
+    baseUrl: 'http://localhost:3000',
+    watchForFileChanges: false,
     specPattern: 'cypress/e2e/**/*.cy.{ts,js}',
     supportFile: 'cypress/support/index.ts',
     setupNodeEvents(on, config) {
--- cypress/e2e/Homepage.cy.ts
+++
@@ -1,8 +0,0 @@
-describe('First test', () => {
-    it('Should visit homepage', () => {
-        cy.visit('/')
-        it('Should open and check burger button', () => {
-        cy.get('.index-module-scss-module__fXNova__burger > svg > path').click()
-    })
-    })
-})
