8bcd28d40cddce87baccbaa73fbeaee919a0fc23
Karen <kmkrtchyan335@gmail.com>
2025-06-11 18:51:55 +0400

    New commit

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     watchForFileChanges: false,
+    defaultCommandTimeout: 10000,
     specPattern: 'cypress/e2e/**/*.cy.{ts,js}',
     supportFile: 'cypress/support/index.ts',
     setupNodeEvents(on, config) {
